212979fb82e3f9a081aa5f6338721c6ac7d2e397
ChrisLommerse12 <89691283+Chris-17Lommerse@users.noreply.github.com>
2025-10-01 20:16:05 +0200

    feat: Class User en Enum voor EmployeeRole

---
+++ Ticket_System_TheGardenGroup/Ticket_System_TheGardenGroup/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Ticket_System_TheGardenGroup.Models
+{
+    public class Employee
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public int EmployeeNumber { get; set; } = 0;
+        public string Password { get; set; } = "";
+        public EmployeeRole EmployeeRole { get; set; } = EmployeeRole.RegularEmployee;
+        public string Name { get; set; } = "";
+        public string Surname { get; set; } = "";
+        public string EmailAddress { get; set; } = "";
+    }
+}
