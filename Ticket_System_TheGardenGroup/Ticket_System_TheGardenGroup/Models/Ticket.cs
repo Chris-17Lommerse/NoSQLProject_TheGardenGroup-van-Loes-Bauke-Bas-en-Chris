aeaeb461fa3ca00e117d6adc2c6b6e1cccfe37d5
ChrisLommerse12 <89691283+Chris-17Lommerse@users.noreply.github.com>
2025-10-01 21:23:21 +0200

    .

--- Ticket_System_TheGardenGroup/Ticket_System_TheGardenGroup/Models/Ticket.cs
+++ Ticket_System_TheGardenGroup/Ticket_System_TheGardenGroup/Models/Ticket.cs
@@ -13,6 +13,9 @@ namespace Ticket_System_TheGardenGroup.Models
         public string TicketId { get; set; } = "";
         public DateTime CreationTime { get; set; } = DateTime.Now;
         public TicketStatus TicketStatus { get; set; } = TicketStatus.Open;
-
+        public string TicketName { get; set; } = "";
+        public string Description { get; set; } = "";
+        public bool IsSolved { get; set; } = false;
+        public string TicketEscalationDescription { get; set; } = "";
     }
 }
