428a06a08bbbd320694e517339ae585ab34fec05
Bas Raadsveld <bcraadsveld@gmail.com>
2025-10-09 16:23:16 +0200

    Revert "push"

--- Ticket_System_TheGardenGroup/Ticket_System_TheGardenGroup/Models/EmbeddedEmployee.cs
+++ Ticket_System_TheGardenGroup/Ticket_System_TheGardenGroup/Models/EmbeddedEmployee.cs
@@ -8,13 +8,10 @@ namespace Ticket_System_TheGardenGroup.Models
     {
         [BsonElement("employee_number")]
         public int EmployeeNumber { get; set; } = 0;
-
         [BsonElement("employee_role")]
         public EmployeeRole EmployeeRole { get; set; } = EmployeeRole.REGULAR_EMPLOYEE;
-
         [BsonElement("emailaddress")]
         public string EmailAddress { get; set; } = "";
-
         [BsonElement("name")]
         public string Name { get; set; } = "";
     }
