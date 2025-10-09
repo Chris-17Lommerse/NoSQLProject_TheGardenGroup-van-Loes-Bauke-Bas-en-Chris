a0e4172fa6327a9825ca175cea73983563c8c710
Mando75317 <bcraadsveld@gmail.com>
2025-10-09 16:20:19 +0200

    push

--- Ticket_System_TheGardenGroup/Ticket_System_TheGardenGroup/Program.cs
+++ Ticket_System_TheGardenGroup/Ticket_System_TheGardenGroup/Program.cs
@@ -10,40 +10,35 @@ namespace Ticket_System_TheGardenGroup
     {
         public static void Main(string[] args)
         {
-            // Laad .env vóór configuratie
+
             DotNetEnv.Env.TraversePath().Load();
 
             var builder = WebApplication.CreateBuilder(args);
 
-            // Lees connectiegegevens
             var mongoConnectionString = Environment.GetEnvironmentVariable("Mongo__ConnectionString");
             var databaseName = Environment.GetEnvironmentVariable("Mongo__Database") ?? "TheGardenGroup"; // fallback
 
             if (string.IsNullOrWhiteSpace(mongoConnectionString))
                 throw new InvalidOperationException("Mongo__ConnectionString is niet ingesteld in .env");
 
-            // MongoClient als Singleton
             builder.Services.AddSingleton<IMongoClient>(sp =>
             {
                 return new MongoClient(mongoConnectionString);
             });
 
-            // 2. Database als Scoped service
             builder.Services.AddScoped(sp =>
             {
                 var client = sp.GetRequiredService<IMongoClient>();
                 return client.GetDatabase(databaseName);
             });
 
-            // 3. Repositories
             builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
             builder.Services.AddScoped<ITicketRepository, TicketRepository>();
 
-            // 4. Services
             builder.Services.AddScoped<ITicketService, TicketService>();
             builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 
-            // 5. MVC controllers
+
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
