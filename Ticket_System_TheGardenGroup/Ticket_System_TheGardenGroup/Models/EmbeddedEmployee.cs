5c7eb1fe27db9403abcdc0552dcb3bea6c9f3fa9
ChrisLommerse12 <89691283+Chris-17Lommerse@users.noreply.github.com>
2025-10-11 10:57:15 +0200

    feat: Tickets werken nu

--- Ticket_System_TheGardenGroup/Ticket_System_TheGardenGroup/Models/EmbeddedEmployee.cs
+++ Ticket_System_TheGardenGroup/Ticket_System_TheGardenGroup/Models/EmbeddedEmployee.cs
@@ -7,12 +7,18 @@ namespace Ticket_System_TheGardenGroup.Models
     public class EmbeddedEmployee
     {
         [BsonElement("employee_number")]
-        public int EmployeeNumber { get; set; } = 0;
+        public EmployeeNumber EmployeeNumber { get; set; } = new EmployeeNumber();
         [BsonElement("employee_role")]
         public EmployeeRole EmployeeRole { get; set; } = EmployeeRole.REGULAR_EMPLOYEE;
         [BsonElement("emailaddress")]
         public string EmailAddress { get; set; } = "";
         [BsonElement("name")]
         public string Name { get; set; } = "";
+        [BsonElement("surname")]
+        public string Surname { get; set; } = "";
+        [BsonElement("password")]
+        public string Password { get; set; } = "";
+        [BsonElement("isActive")]
+        public bool IsActive { get; set; } = false;
     }
 }
