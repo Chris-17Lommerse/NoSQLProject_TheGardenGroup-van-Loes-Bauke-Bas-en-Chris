122a394fb5e9376d39024d46953894554dc79d3d
Bas Raadsveld <bcraadsveld@gmail.com>
2025-10-11 15:53:51 +0200

    Bug fixing

--- Ticket_System_TheGardenGroup/Ticket_System_TheGardenGroup/Models/Employee.cs
+++ Ticket_System_TheGardenGroup/Ticket_System_TheGardenGroup/Models/Employee.cs
@@ -11,15 +11,15 @@ namespace Ticket_System_TheGardenGroup.Models
         [BsonElement("_id")]
         public string? Id { get; set; }
 
-        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonRepresentation(BsonType.Int32)]
         [BsonElement("employee_number")]
-        public string EmployeeNumber { get; set; } = "";
+        public int EmployeeNumber { get; set; } = 0;
 
         [BsonElement("password")]
         public string Password { get; set; } = "";
 
         [BsonElement("employee_role")]
-        public EmployeeRole EmployeeRole { get; set; } = EmployeeRole.REGULAR_EMPLOYEE;
+        public EmployeeRole EmployeeRole { get; set; } = EmployeeRole.Regular_employee;
 
         [BsonElement("name")]
         public string Name { get; set; } = "";
@@ -29,5 +29,8 @@ namespace Ticket_System_TheGardenGroup.Models
 
         [BsonElement("emailaddress")]
         public string EmailAddress { get; set; } = "";
+
+        [BsonElement("isActive")]
+        public bool IsActive { get; set; } = false;
     }
 }
