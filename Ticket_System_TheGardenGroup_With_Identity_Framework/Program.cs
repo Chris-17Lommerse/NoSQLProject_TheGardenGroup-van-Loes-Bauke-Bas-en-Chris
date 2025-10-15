9b4ce8916d0c52945763d85c2bffff2004693051
ChrisLommerse12 <89691283+Chris-17Lommerse@users.noreply.github.com>
2025-10-15 19:20:47 +0200

    Login settings geconfigureeerd

--- Ticket_System_TheGardenGroup_With_Identity_Framework/Program.cs
+++ Ticket_System_TheGardenGroup_With_Identity_Framework/Program.cs
@@ -18,6 +18,29 @@ namespace Ticket_System_TheGardenGroup_With_Identity_Framework
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddRazorPages();
+
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+            });
+
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
@@ -39,6 +62,7 @@ namespace Ticket_System_TheGardenGroup_With_Identity_Framework
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
