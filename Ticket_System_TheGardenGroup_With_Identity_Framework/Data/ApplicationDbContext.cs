e8083402cb72107130bbcc461e3dd0c033c83227
ChrisLommerse12 <89691283+Chris-17Lommerse@users.noreply.github.com>
2025-10-15 18:58:42 +0200

    feat: Applicatie aangemaakt met Identity framework

---
+++ Ticket_System_TheGardenGroup_With_Identity_Framework/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ticket_System_TheGardenGroup_With_Identity_Framework.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
